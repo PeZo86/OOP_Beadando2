fad52d1573d3f9c792bd48b9d298be612a3ff464
PeZo86 <varga.zoltan.peter@hotmail.com>
2025-04-16 21:38:06 +0200

    some other improvements

--- SimpleBookingApp/functions.py
+++ SimpleBookingApp/functions.py
@@ -1,70 +1,57 @@
-def view_available_flights(self):
-    print("\nAvailable flights:")
+class Flight:
+    def __init__(self):
+        self.flight = {
+            101: {'arrival': 'Rome', 'price': 15000, 'booked': False, 'guest': None},
+            102: {'arrival': 'Catania', 'price': 25000, 'booked': False, 'guest': None},
+            201: {'arrival': 'Paris', 'price': 23500, 'booked': False, 'guest': None},
+            310: {'arrival': 'London', 'price': 35000, 'booked': False, 'guest': None},
+            501: {'arrival': 'Debrecen', 'price': 5000, 'booked': False, 'guest': None},
+            502: {'arrival': 'Szeged', 'price': 7500, 'booked': False, 'guest': None},
+        }
 
-    for flight_no, details in self.flight.keys():
+    def view_available_flights(self):
+        print("\nElérhető járatok")
 
-        if not details['booked']:
-            print(f"Flight {flight_no}: {details['arrival']} - {details['price']}")
+        for flight_no, details in self.flight.keys():
 
-def book_flight(self, flight_no):
+            if not details['booked']:
+                print(f"Járat adatai: {flight_no}: {details['arrival']} - {details['price']}")
 
-    self.view_available_flights()
+    def book_flight(self, flight_no):
+        self.view_available_flights()
+        try:
+            flight_no = int(input("Enter flight no: "))
+            if flight_no in self.flight.keys() and not self.flight[flight_no]['booked']:
+                guest_name = input("Enter guest name: ")
+                self.flight[flight_no]['booked'] = True
+                self.flight[flight_no]['guest'] = guest_name
+                print(f"\nA járat {flight_no} sikeresen lefoglalva az alábbi néven: {guest_name}.")
 
-    try:
+            else:
+                print("\nA járat nem elérhető vagy már le lett foglalva.")
 
-        flight_no = int(input("Enter flight no: "))
+        except ValueError:
+            print("\nHibás járatszám. Adjon meg egy érvényes járatszámot")
 
-        if flight_no in self.flight.keys() and not self.flight[flight_no]['booked']:
+    def check_flight(self, flight_no):
+        guest_name = input("\nAdja meg a neve a járat ellenőrzéséhez ")
+        found = False
+        for flight_no, details in self.flight.items():
+            if details['booked'] and details['guest'] == guest_name:
+                print(f"\n{guest_name} foglalt az alábbi járatra {flight_no}: {details['arrival']} - {details['price']}")
+                found = True
+                break
 
-            guest_name = input("Enter guest name: ")
+        if not found:
+            print("\nNincs foglalás az alábbi néven {guest_name}.")
 
-            self.flight[flight_no]['booked'] = True
+    def cancel_flight(self, flight_no):
+        guest_name = input("\nAdja meg a nevet a törléshez: ")
+        for flight_no, details in self.flight.items():
+            if details['booked'] and details['guest'] == guest_name:
+                self.flight[flight_no]['booked'] = False
+                self.flight[flight_no]['guest'] = None
+                print(f"\nAz alábbi járat {flight_no} törölve lett az alábbi utas nevéről {guest_name}.")
+                return
 
-            self.flight[flight_no]['guest'] = guest_name
-
-            print(f"\nFlight {flight_no} successfully booked for {guest_name}.")
-
-        else:
-
-            print("\nFlight is either not available or already booked.")
-
-    except ValueError:
-
-        print("\nInvalid flight number. Please enter a valid flight number.")
-
-def check_flight(self, flight_no):
-
-    guest_name = input("\nEnter guest name to check booking: ")
-
-    found = False
-
-    for flight_no, details in self.flight.items():
-
-        if details['booked'] and details['guest'] == guest_name:
-
-            print(f"\n{guest_name} has booked for {flight_no}: {details['arrival']} - {details['price']}")
-
-            found = True
-
-            break
-
-    if not found:
-        print("\nNo flight booking for {guest_name}.")
-
-def cancel_flight(self, flight_no):
-
-    guest_name = input("\nEnter guest name to cancel: ")
-
-    for flight_no, details in self.flight.items():
-
-        if details['booked'] and details['guest'] == guest_name:
-
-            self.flight[flight_no]['booked'] = False
-
-            self.flight[flight_no]['guest'] = None
-
-            print(f"\nFlight {flight_no} cancelled for {guest_name}.")
-
-            return
-
-        print("\nNo flight booking for {guest_name}.")
+            print("\nNincs foglalás az alábbi néven {guest_name}.")
--- SimpleBookingApp/main.py
+++ SimpleBookingApp/main.py
@@ -1,109 +1,78 @@
-"""
-class Flight:
-
-    def __init__(self):
-
-        self.flight = {
-
-            101: {'arrival': 'Rome', 'price': 15000, 'booked': False, 'guest': None},
-            102: {'arrival': 'Catania', 'price': 25000, 'booked': False, 'guest': None},
-            201: {'arrival': 'Paris', 'price': 23500, 'booked': False, 'guest': None},
-            310: {'arrival': 'London', 'price': 35000, 'booked': False, 'guest': None},
-            501: {'arrival': 'Debrecen', 'price': 5000, 'booked': False, 'guest': None},
-            502: {'arrival': 'Szeged', 'price': 7500, 'booked': False, 'guest': None},
-        }
-
-"""
-
 from abc import ABC, abstractmethod
 
 class Flight(ABC):
 
     def __init__(self, flight_no, arrival, price):
-
         self.flight_no = flight_no,
-
         self.arrival = arrival,
-
         self.price = price,
 
     def flight_type(self):
-
         pass
 
     def __repr__(self):
-
         return f"{self.flight_type()} Járat #{self.flight_no} -> {self.arrival}, Ár: {self.price}"
 
+#öröklik a járat osztályt és megvalósítják a járat típust
 class NationalFlight(Flight):
 
     def flight_type(self):
-
         return "Belföldi"
 
 class InternationalFlight(Flight):
 
     def flight_type(self):
-
         return "Nemzetközi"
 
+# tárolja a járatokat és azok nevét
+class FlightCompany:
 
+    def __init__(self, nev):
+        self.nev = nev
+        self.flights = []
 
+    def add_flight(self, flight):
+        self.flights.append(flight)
 
-    def run(self):
+    def total_flight(self):
+        return self.flights
+
+    def __repr__(self):
+        return f"Légi társaság: {self.nev}, Járat száma: {len(self.flights)}"
+
+from functions import Flight
+
+    def main():
+        flight_manager = Flight()
 
         while True:
-
-            print("\nWelcome to Simple Booking App")
-
-            print("1. View available flights")
-
-            print("2. Book flight")
-
-            print("3. Check flight")
-
-            print("4. Cancel flight")
-
-            print("5. Exit")
+            print("\nÜdvözlöm a Simple Booking App-ban")
+            print("1. Mutasd az elérhető járatokat")
+            print("2. Járat foglalása")
+            print("3. Járat ellenőrzése")
+            print("4. Járat törlése")
+            print("5. Kilépés")
 
             try:
-
-                choice = int(input("Enter your choice: "))
-
+                choice = int(input("Adja meg mit szeretne: "))
                 if choice == 1:
-
                     self.view_available_flights()
-
                 elif choice == 2:
-
                     self.book_flight()
-
                 elif choice == 3:
-
                     self.check_flight()
-
                 elif choice == 4:
-
                     self.cancel_flight()
-
                 elif choice == 5:
-
-                    print("\nThank you for using Simple Booking App. Goodbye!")
-
+                    print("\nKöszönjük hogy a Simple Booking App-ot használta. Viszlát")
                     break
-
                 else:
-
-                    print("\nInvalid choice. Please enter a valid choice.")
+                    print("\nHbiás választás. Kérem válasszon egy helyes számot")
 
             except ValueError:
-
-                print("\nInvalid input. Please enter a number between 1 and 5.")
+                print("\nHibás szám. Kérem válasszon 1 és 5 között egy számot")
 
 if __name__ == '__main__':
-
-    flight = Flight()
-
-    flight.run()
+    main()
 
 
